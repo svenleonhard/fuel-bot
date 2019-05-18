47f642f46d1cf6b8c1996c52e1a2b4aaa8908b7c
svenleonhard <40168082+svenleonhard@users.noreply.github.com>
2019-05-18 19:21:40 +0200

    Add data class for user fuel information

---
+++ userFuelInformation.js
@@ -0,0 +1,9 @@
+class UserFuelInformation {
+    constructor(fuelType, city, distance) {
+        this.fuelType = fuelType;
+        this.city = city;
+        this.distance = distance;
+    }
+}
+
+module.exports.UserFuelInformation = UserFuelInformation;
